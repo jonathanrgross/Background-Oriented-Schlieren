1aad130cf09d813d8fcc4d4d6fe5dbb3a9d2cd23
Jonathan R Gross <jonathanrgross@gmail.com>
2016-09-02 13:16:19 -0700

    adding latest changes

--- practice_files/openvideo.py
+++ practice_files/openvideo.py
@@ -19,10 +19,19 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-cam = cv2.VideoCapture('street.avi')
+#cam = cv2.VideoCapture('street.avi')
+cam = cv2.VideoCapture('/home/jack/Pictures/2016-08-05_flame/DSC_0023.MOV')
 print cam.isOpened()
 
-vid = np.zeros((600,360,3,80))
+width = cam.get(4)
+height = cam.get(3)
+if cam.get(0) > 0:
+    nFrames = cam.get(0)
+else:
+    nFrames = 10
+vidData = [width, height, nFrames]
+
+vid = np.zeros((width,height,3,nFrames))
 frameNum = 0
 while(True):
         ret, frame = cam.read()
@@ -47,6 +56,49 @@ cv2.destroyAllWindows()
 
 
 #%%
+
+# create videocapture object
+filename = '/home/jack/Pictures/2016-08-05_flame/DSC_0023.MOV'
+cam = cv2.VideoCapture(filename)
+
+# check if the videocapture object was successfully created
+if cam.isOpened() == 'False':
+    print('error reading ' + str(filename))
+
+# get video dimensions
+width = cam.get(4)
+height = cam.get(3)
+if cam.get(0) > 0:
+    nFrames = cam.get(0)
+else:
+    nFrames = 10
+vidData = [width, height, nFrames]
+# NOTE: RIGHT NOW THE METHOD I USE TO CONVERT VIDEO DOESNT KEEP METADATA ON NFRAMES I THINK
+vid = np.zeros((width,height,3,nFrames))
+
+
+for frameNum in range(nFrames):
+    ret, frame = cam.read()
+    
+    a = np.array(frame[:,:,:])
+    a2 = np.expand_dims(a, 4)
+    vid[:,:,:,frameNum] = a2[:,:,:,0]
+
+    
+    print frameNum
+    print ret
+    
+    #cv2.imshow('frame', frame)
+    #if cv2.waitKey(10) & 0xFF == ord('q'):
+    #    break
+    #cam.release()
+    #cv2.destroyAllWindows()
+
+# announce that the video was read successfully
+print('successfully read 20 frames')
+#plt.figure(), plt.imshow(Vid[])
+
+
 
 
 
